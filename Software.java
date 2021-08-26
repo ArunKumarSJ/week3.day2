15d94fad931b43b54e2eb351c2879af2fb6b1bf8
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-26 12:30:48 +0530

    Day 6 Assignment

---
+++ Software.java
@@ -0,0 +1,19 @@
+package week3.Day2;
+
+public interface Software {
+	/**
+	 * 
+	 * @param name
+	 */
+	 void softwareResouces(String name);
+	 /**
+	  * 
+	  * @param name
+	  */
+	 void softwareResouces1(String name);
+	 /**
+	  * 
+	  * @param name
+	  */
+	 void softwareResouces2(String name);
+}
