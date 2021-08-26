507a959b3606488c329feb7a5d9614d67860962e
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-26 12:30:04 +0530

    Day 6 Assignment

---
+++ Hardware.java
@@ -0,0 +1,15 @@
+package week3.Day2;
+
+public interface Hardware {
+	/**
+	 * 
+	 * @param name
+	 */
+	 void hardwareResouces(String name);
+	/**
+	 * 
+	 * @param name
+	 */
+	 void hardwareResouces1(String name);
+	 
+}
